7de47bc98cfadb9cac6de0b1bad16f0aa351ab5a
jeffrey1hu <hu1chao@126.com>
2017-09-16 11:43:42 +0800

    config the abs path of source data

---
+++ collect_data.sh
@@ -0,0 +1,7 @@
+
+
+cd /mnt
+
+wget http://ai-challenger-scene.cn-bj.ufileos.com/ai_challenger_scene_train_20170904.zip
+
+unzip ai_challenger_scene_train_20170904.zip
---
+++ config.py
@@ -0,0 +1,5 @@
+
+
+TRAIN_DATA_PATH = '/mnt/ai_challenger_caption_train_20170902'
+VAL_DATA_PATH = '/mnt/ai_challenger_caption_validation_20170910'
+VGG_MODEL_PATH = '/mnt/image_caption/show-attend-and-tell/data/imagenet-vgg-verydeep-19.mat'
