27e1bed660511a050bbb314c1bf2ab4129740b72
jeffrey1hu <hu1chao@126.com>
2017-09-22 00:16:34 +0800

    debug

--- train.py
+++ train.py
@@ -17,13 +17,13 @@ def main():
     # load val dataset to print out bleu scores every epoch
     val_data = load_coco_data(data_path='./data', split='val')
 
-    model = CaptionGenerator(word_to_idx, dim_feature=[196, 512], dim_embed=512,
-                                       dim_hidden=800, n_time_step=16, prev2out=True,
+    model = CaptionGenerator(word_to_idx, dim_feature=[196, 512], dim_embed=800,
+                                       dim_hidden=1024, n_time_step=16, prev2out=True,
                                                  ctx2out=True, alpha_c=1.0, selector=True, dropout=True)
 
     solver = CaptioningSolver(model, data, val_data, n_epochs=20, batch_size=128, update_rule='adam',
                                           learning_rate=0.001, print_every=1000, save_every=1, image_path='./image/',
-                                    pretrained_model=None, model_path='model/lstm3/', test_model='model/lstm/model-10',
+                                    pretrained_model=None, model_path='model/lstm4/', test_model='model/lstm/model-10',
                                      print_bleu=True, log_path='log/')
     solver.train()
 
@@ -40,4 +40,9 @@ if __name__ == '__main__':
 # version 2
 #     model = CaptionGenerator(word_to_idx, dim_feature=[196, 512], dim_embed=512,
 #                                        dim_hidden=1500, n_time_step=16, prev2out=True,
+#                                                  ctx2out=True, alpha_c=1.0, selector=True, dropout=True)
+
+# version 3
+#     model = CaptionGenerator(word_to_idx, dim_feature=[196, 512], dim_embed=512,
+#                                        dim_hidden=800, n_time_step=16, prev2out=True,
 #                                                  ctx2out=True, alpha_c=1.0, selector=True, dropout=True)
